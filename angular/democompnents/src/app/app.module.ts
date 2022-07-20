cafa75bcabc36935b698ebd9629612f38064913e
jounikivi <jounikivipera@gmail.com>
2022-07-20 10:42:52 +0300

    lisäyksiä

--- angular/democompnents/src/app/app.module.ts
+++ angular/democompnents/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemocomponentComponent } from './democomponent/democomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemocomponentComponent
   ],
   imports: [
     BrowserModule,
